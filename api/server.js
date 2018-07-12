6e9a09e0d3e685ef3d72c61f3db2da68cb51c70e
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-07-12 23:49:13 +1000

    index.ejs added and renders, but doesnt display quite as intended

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 
-app.get("/", function(req, res) {
-  res.send("Hello World!")
+app.get('/', function (req, res) {
+  // NEW CODE
+  res.render('index');
 })
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!")
+app.listen(7000, function () {
+  console.log("Example app listening on port 7000!")
 })
+
+app.set('view engine', 'ejs')
