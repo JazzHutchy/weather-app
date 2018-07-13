c7148b2a6b24660c1c4e0aff63fecf35b30362bc
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-07-13 17:32:17 +1000

    add style.css, not being recognised by the server for some reason

--- api/server.js
+++ api/server.js
@@ -1,13 +1,20 @@
 const express = require("express")
 const app = express()
 
+// Allow access to 'Public' folder with CSS file (express doesn't allow this by default)
+app.use(express.static('public'));
+// Setup template engine for EJS
+app.set('view engine', 'ejs')
+
+
+// Render the 'index' Embedded JS file
 app.get('/', function (req, res) {
   // NEW CODE
   res.render('index');
 })
 
+// Server location
 app.listen(7000, function () {
   console.log("Example app listening on port 7000!")
 })
 
-app.set('view engine', 'ejs')
