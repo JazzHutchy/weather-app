94680d79fbcf5d2ad0943c3d5376ff592a7540ff
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-07-15 23:50:19 +1000

    Css customisation to provide appropriate background for the app

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 
+// Body Parser Middleware
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
