6defce9ff5c820d490d7959595d992f2120f144d
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-07-17 23:24:12 +1000

    Add ability to display weather w/ post function & error handling

--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const app = express()
+const request = require('request')
+const apiKey = '*************'
 
 // Body Parser Middleware
 const bodyParser = require('body-parser');
@@ -20,6 +22,24 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   res.render('index');
   console.log(req.body.city);
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render('index', { weather: null, error: 'Error, please try again' })
+    } else {
+      let weather = JSON.parse(body)
+
+      if (weather.main == undefined) {
+        res.render('index', { weather: null, error: 'Error, please try again' })
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
+        res.render('index', { weather: weatherText, error: null })
+      }
+    }
+
+  }
+  })
 })
 
 // Server location
