79d2fb0fdeceb9479b5b260b27a491131b6d1d11
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-07-22 15:29:15 +1000

    Fixed critical error: res.render was being called twice, preventing POST requests from working

--- api/server.js
+++ api/server.js
@@ -12,35 +12,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Setup template engine for EJS
 app.set('view engine', 'ejs')
 
-
 // GET: Render the 'index' Embedded JS file
 app.get('/', function (req, res) {
   res.render('index', { weather: null, error: null });
 })
 
 // POST
-app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city);
+app.post("/", function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   request(url, function (err, response, body) {
     if (err) {
-      res.render('index', { weather: null, error: 'Error, please try again' })
+      res.render("index", { weather: null, error: "Error, please try again" });
     } else {
-      let weather = JSON.parse(body)
-
+      let weather = JSON.parse(body);
       if (weather.main == undefined) {
-        res.render('index', { weather: null, error: 'Error, please try again' })
+        res.render("index", {
+          weather: null,
+          error: "Error, please try again"
+        });
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`
-        res.render('index', { weather: weatherText, error: null })
+        let weatherText = `It's ${weather.main.temp} degrees in ${
+          weather.name
+          }!`;
+        console.log(weatherText);
+        res.render("index", { weather: weatherText, error: null });
       }
     }
-
-  }
-  )
-})
+  });
+});
 
 // Server location
 app.listen(7000, function () {
